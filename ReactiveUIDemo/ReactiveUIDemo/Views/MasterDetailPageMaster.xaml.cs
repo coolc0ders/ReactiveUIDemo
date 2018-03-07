522129b2059e5c8b1a2c3f5eb3a26c507378f3b3
Damien <damientohin@gmail.com>
2018-03-07 13:32:57 +0100

    Initial commit

---
+++ ReactiveUIDemo/ReactiveUIDemo/Views/MasterDetailPageMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReactiveUIDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterDetailPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterDetailPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterDetailPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterDetailPageMenuItem> MenuItems { get; set; }
+            
+            public MasterDetailPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterDetailPageMenuItem>(new[]
+                {
+                    new MasterDetailPageMenuItem { Id = 0, Title = "Page 1" },
+                    new MasterDetailPageMenuItem { Id = 1, Title = "Page 2" },
+                    new MasterDetailPageMenuItem { Id = 2, Title = "Page 3" },
+                    new MasterDetailPageMenuItem { Id = 3, Title = "Page 4" },
+                    new MasterDetailPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ ReactiveUIDemo/ReactiveUIDemo/Views/MasterDetailPageMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveUIDemo.Views
+{
+
+    public class MasterDetailPageMenuItem
+    {
+        public MasterDetailPageMenuItem()
+        {
+            TargetType = typeof(MasterDetailPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
