4e7c3d7a50efe6e42770749ff9efb8e8e8a6ba8a
Damien <damientohin@gmail.com>
2018-03-08 10:32:18 +0100

    I added content page base

---
+++ ReactiveUIDemo/ReactiveUIDemo/Views/ContentPageBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI.XamForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveUIDemo.Views
+{
+    public class ContentPageBase<TViewModel> : ReactiveContentPage<TViewModel> where TViewModel : class
+    {
+    }
+}
--- ReactiveUIDemo/ReactiveUIDemo/Views/LoginPage.xaml.cs
+++ ReactiveUIDemo/ReactiveUIDemo/Views/LoginPage.xaml.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms.Xaml;
 namespace ReactiveUIDemo.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LoginPage : ContentPage
+	public partial class LoginPage : ContentPageBase<LoginViewModel>
 	{
 		public LoginPage ()
 		{
